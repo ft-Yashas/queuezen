10cfa091a1e2175cbb097e174d3a56694ce9c408
ft-Yashas <ft.yashas@gmail.com>
2026-01-31 11:50:09 +0530

    QueueZen full-stack implementation

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE"],
+  }));
 
 
 const healthRoute = require("./routes/health");
